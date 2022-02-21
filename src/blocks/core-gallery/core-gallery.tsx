b2f33df9398f0d05142d31344bbf6ab9172b29f7
modemlooper <modemlooper@gmail.com>
2022-02-21 09:14:01 -0800

    gallery update

--- src/blocks/core-gallery/core-gallery.tsx
+++ src/blocks/core-gallery/core-gallery.tsx
@@ -1,4 +1,4 @@
-import { Component, Host, h, ComponentInterface, Prop } from '@stencil/core';
+import { Component, Host, h, ComponentInterface, Prop, Element } from '@stencil/core';
 
 @Component({
   tag: 'core-gallery',
@@ -6,16 +6,40 @@ import { Component, Host, h, ComponentInterface, Prop } from '@stencil/core';
 })
 export class CoreGallery implements ComponentInterface {
 
+  @Element() el: HTMLElement;
   @Prop() data: any;
 
   componentWillLoad(): void | Promise<void> {
       console.log(this.data);
   }
 
+  processHTML() {
+ 
+    const div = document.createElement('div');
+    div.setAttribute('class', 'appp-gallery');
+    
+
+    this.data && this.data.innerBlocks.map( item => {
+
+      const domParser = new DOMParser();
+      const doc = domParser.parseFromString(item.innerHTML, "text/html");
+      const img = doc.querySelector('img');  
+      const slide = document.createElement('div');
+
+      slide.setAttribute('style', `background-image: url(${img.src}); background-size: cover; background-position: center;`);
+      slide.setAttribute('class', 'app-slide');
+      
+      div.appendChild(slide);
+    })
+
+    this.el.appendChild(div);
+
+  }
+
   render() {
     return (
-      <Host class="ion-padding" innerHTML={ this.data.innerHTML }>
-        gallery broken
+      <Host>
+        {this.processHTML()}
       </Host>
     );
   }
